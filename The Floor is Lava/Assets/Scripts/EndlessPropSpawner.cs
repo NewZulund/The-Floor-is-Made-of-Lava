2f0b7397156a23cae99eb9df3c3ff45f1abe99d1
Isaac Blomfield <colias@live.com>
2015-04-11 11:30:19 +1200

    Moved scripts to scripts folder

---
+++ The Floor is Lava/Assets/Scripts/EndlessPropSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndlessPropSpawner : MonoBehaviour {
+
+	//Prop Variables
+	public PropGroup[] propGroups;
+	
+	//Rail Variables
+	public RunningRail[] rails;
+	
+	//Spawn Wave Variables
+	public float spawnMin = 1f;
+	public float spawnMax = 3f;
+
+	void Start()
+	{
+		Spawn();
+	}
+
+	void Spawn()
+	{
+		//Select a rail to spawn on. 
+		PropGroup selectedPropGroup = propGroups[Random.Range(0,propGroups.Length)];
+		RunningRail selectedRail = rails[Random.Range(0,rails.Length)];	
+
+		GameObject objectToSpawn = selectedPropGroup.getRandomAsset();
+		Instantiate(objectToSpawn, selectedRail.startPosition.position, Quaternion.identity);
+		Invoke("Spawn", Random.Range(spawnMin, spawnMax));
+	}
+
+}
