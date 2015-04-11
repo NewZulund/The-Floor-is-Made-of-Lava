31e27e9ad5d082a01c34f7b8276aebb0181f04aa
Isaac Blomfield <colias@live.com>
2015-04-11 12:46:34 +1200

    Created classes for platform spawning

---
+++ The Floor is Lava/Assets/EndlessPlatform.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndlessPlatform : MonoBehaviour {
+
+	public float length = 2.8f;
+	public GameObject
+}
--- The Floor is Lava/Assets/Scripts/PropGroup.cs
+++ The Floor is Lava/Assets/Scripts/PropGroup.cs
@@ -1,19 +1,71 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PropGroup : MonoBehaviour
 {
-	//Prop Variables
-	public GameObject[] assets; 
 
-	//Group Variables
-	public float length = 100f;
+	//Prefab Variables
+	public GameObject[] platformPrefabs;
 
-	
-	public GameObject getRandomAsset ()
+	//Instantiated Variables
+	public List<GameObject> inactivePlatforms = new List<GameObject>(); //Instantiated but not in use
+	public List<GameObject> activePlatforms = new List<GameObject>(); //In use
+	public float platformSeperation = 2.5f;
+
+
+	//Instantiation Variables
+	public int INSTANTIATED_PLATFORM_COUNT = 20;
+	public Vector3 storagePosition;
+
+	void Awake()
 	{
-		//TODO make less random
-		return assets[Random.Range(0, assets.Length)];
+
+		//TODO instantiate various prefabs
+
+		//Spawn even amounts of prefabs.
+		for(int i = 0; i < INSTANTIATED_PLATFORM_COUNT; i++){
+			inactivePlatforms.Add(Instantiate(platformPrefabs[0], storagePosition, Quaternion.identity) as GameObject);
+		}
+	}
+	
+	private GameObject getPlatformOfIndex (int index)
+	{
+		if(index >= platformPrefabs.Length)
+		{
+			index = 0;
+			Debug.Log("Platform doesn't exist at that index!");
+		}
+
+		//Get platform from pool or instantiate new if pool is empty
+		GameObject platform;
+		if(inactivePlatforms.Count == 0)
+		{
+			platform = Instantiate(platformPrefabs[index], storagePosition, Quaternion.identity) as GameObject;
+		}
+		else
+		{
+			platform = inactivePlatforms[0];
+			inactivePlatforms.RemoveAt(0);
+		}
+
+		activePlatforms.Add(platform);
+		return platform;
+	}
+
+	public void spawnPlatformSet (RunningRail selectedRail)
+	{
+
+		//TODO make length of platform group random
+		for(int i = 0; i < 5; i++)
+		{
+			//TODO use more than one platform asset if possible
+			GameObject platform = getPlatformOfIndex(0);
+
+			//Offset by platform count
+			platform.transform.position = selectedRail.position + (Vector3.back * platformSeperation * i);
+
+		}
 	}
 }
 
