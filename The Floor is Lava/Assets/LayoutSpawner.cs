7144ab6ae4ab12d7a79d55d598f851ecee9d75e6
Isaac Blomfield <colias@live.com>
2015-05-07 12:41:46 +1200

    Add platform layout loader and spawner

---
+++ The Floor is Lava/Assets/LayoutSpawner.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class LayoutSpawner : MonoBehaviour {
+
+	public string layoutFileName = "Assets/layout_files/layouts.csv";
+
+	LayoutLoader loader;
+	public List<PlatformLayout> layouts;
+
+	public Dictionary<int, PlatformLayout> layoutByIndex;
+	public Dictionary<int, List<PlatformLayout>> layoutByIncomingRail;
+
+
+	void Awake () 
+	{
+		loader = GetComponent<LayoutLoader>();
+		layouts = loader.Load(layoutFileName);
+		layoutByIncomingRail = buildIncomingRailDictionary();
+
+	}
+
+	/**
+	 * Builld a dictonary from incoming rails to layouts that can accept those rails. 
+	 * This list is used to spawn plausable layouts for the ends of pre-spawned layouts.
+	 */
+	private Dictionary<int, List<PlatformLayout>> buildIncomingRailDictionary()
+	{
+		Dictionary<int, List<PlatformLayout>> layoutDictionary = new Dictionary<int, List<PlatformLayout>>();
+
+		foreach(PlatformLayout layout in layouts)
+		{
+			for(int i = 0; i < layout.startRails.Length; i++)
+			{
+				if(layoutDictionary.ContainsKey(layout.startRails[i]))
+				{
+					layoutDictionary[layout.startRails[i]].Add(layout);
+				}
+				else
+				{
+					List<PlatformLayout> list = new List<PlatformLayout>();
+					list.Add(layout);
+					layoutDictionary.Add(layout.startRails[i], list);
+				}
+			}
+		}
+
+		return layoutDictionary;
+	}
+
+	public void SpawnLayout(int index)
+	{
+		//TODO
+	}
+}
---
+++ The Floor is Lava/Assets/Scripts/Platform Spawning/LayoutLoader.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using System.Text; 
+
+public class LayoutLoader : MonoBehaviour {
+
+	public List<PlatformLayout> Load(string fileName)
+	{
+
+		List<PlatformLayout> platformLayouts = new List<PlatformLayout>();
+
+		try
+		{
+			string line; 
+			StreamReader reader = new StreamReader(fileName, Encoding.Default);
+
+			using(reader)
+			{
+				//TODO make safer, on fail a single layout
+				line = reader.ReadLine();
+
+				do
+				{
+					PlatformLayout platformLayout = new PlatformLayout();
+
+					string[] lineSplit;
+					lineSplit = line.Split(','); 
+
+					platformLayout.number = int.Parse(lineSplit[0]);
+					platformLayout.name = lineSplit[1];
+
+					int width = int.Parse(lineSplit[2]);
+					int length = int.Parse(lineSplit[3]);
+					platformLayout.width = width;
+					platformLayout.length = length;
+
+					//Parse rail-start line
+					line = reader.ReadLine();
+					lineSplit = line.Split(new char[] {','}, System.StringSplitOptions.RemoveEmptyEntries);
+					platformLayout.startRails = new int[lineSplit.Length];
+
+					for(int i = 0; i < lineSplit.Length; i++)
+					{
+						platformLayout.startRails[i] = int.Parse(lineSplit[i]);
+					}
+
+					//Parse platform array
+					platformLayout.platformArray = new int[length][];
+
+					for(int y = 0; y < length; y++)
+					{
+						line = reader.ReadLine();
+						lineSplit = line.Split(new char[] {','}, System.StringSplitOptions.RemoveEmptyEntries);
+						platformLayout.platformArray[y] = new int[width];
+
+						for(int x = 0; x < width; x++)
+						{
+							platformLayout.platformArray[y][x] = int.Parse(lineSplit[x]);
+						}
+					}
+
+
+					//Parse line-end rails
+					line = reader.ReadLine();
+					lineSplit = lineSplit = line.Split(new char[] {','}, System.StringSplitOptions.RemoveEmptyEntries);
+					platformLayout.endRails = new int[lineSplit.Length];
+					
+					for(int i = 0; i < lineSplit.Length; i++)
+					{
+						platformLayout.endRails[i] = int.Parse(lineSplit[i]);
+					}
+
+					//parse notification array
+					platformLayout.notificationArray = new int[length][];
+				
+					for(int y = 0; y < length; y++)
+					{
+						line = reader.ReadLine();
+						lineSplit = line.Split(','); 
+						platformLayout.platformArray[y] = new int[width];
+						
+						for(int x = 0; x < width; x++)
+						{
+							platformLayout.platformArray[y][x] = int.Parse(lineSplit[x]);
+						}
+					}
+
+					//Skip final line
+					reader.ReadLine();
+					line = reader.ReadLine();
+
+					platformLayouts.Add(platformLayout);
+				}
+				while (line != null);
+			}
+		}
+		catch(UnityException uex)
+		{
+			print (uex.Message);
+		}
+
+		return platformLayouts;
+	}
+
+}
