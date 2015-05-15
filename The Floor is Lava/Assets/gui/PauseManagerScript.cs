f0e07f476b14b260618d6993e86c60a94d8824f1
dalightdj <dalightdj@yahoo.com.au>
2015-05-15 15:35:59 +1200

    Modify menus and Add notifications

--- The Floor is Lava/Assets/gui/PauseManagerScript.cs
+++ The Floor is Lava/Assets/gui/PauseManagerScript.cs
@@ -5,24 +5,31 @@ using System.Collections;
 public class PauseManagerScript : MonoBehaviour {
 
 	public GameObject pauseMenu;
+	public GameObject backgroundMask;
 
 	public void PauseGame(){
 		pauseMenu.SetActive(true);
+		backgroundMask.SetActive (true);
 		Time.timeScale = 0;
 	}
 
 	public void ContinueGame(){
 		pauseMenu.SetActive (false);
-		Time.timeScale = 1;
+		resume ();
 	}
 
 	public void RestartGame(){
 		Application.LoadLevel ("EndlessRunning");
-		Time.timeScale = 1;
+		resume ();
 	}
 
 	public void QuitGame(){
 		Application.LoadLevel ("Home");
+		resume ();
+	}
+
+	private void resume(){
+		backgroundMask.SetActive (false);
 		Time.timeScale = 1;
 	}
 
