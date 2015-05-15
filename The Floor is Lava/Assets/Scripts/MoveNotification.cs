a7ee1b175a750524ad2bef6ae257e61b0baae93f
Guest User <Guest@h-03-07-lab.mddn.vuw.ac.nz>
2015-05-15 15:19:13 +1200

    Add destory notifications

---
+++ The Floor is Lava/Assets/Scripts/MoveNotification.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveNotification : MonoBehaviour {
+	
+	public int moveType;
+	public int position;
+	public float timeOut = 3.0f;
+	public GameObject UIElement;
+
+	void Start()
+	{
+		Invoke ("FadeOut", timeOut);
+	}
+
+	void FadeOut()
+	{
+		Destroy (this.gameObject);
+	}
+
+}
