03759cded2113e93cc6cdd2eff1dfefba7eeccbb
Elizabeth Kim <eliz.kim91@gmail.com>
2015-05-18 07:33:26 +1200

    Added boss monstar
    Added boss monstar

--- The Floor is Lava/Assets/Scripts/EndlessCharacterFailController.cs
+++ The Floor is Lava/Assets/Scripts/EndlessCharacterFailController.cs
@@ -12,8 +12,11 @@ public class EndlessCharacterFailController : MonoBehaviour {
 	public float hurtBounceSpeed = 10.0f;
 	public float hurtModelRevertTime = 1.0f;
 
+	private EndlessController controller;
+
 	void Awake()
 	{
+		controller = GameObject.Find ("EndlessController").GetComponent<EndlessController> ();
 		sadMouth.SetActive(false);
 		happyMouth.SetActive(true);
 		rigidBody =  GetComponent<Rigidbody>();
@@ -23,6 +26,7 @@ public class EndlessCharacterFailController : MonoBehaviour {
 	{
 		if(collision.gameObject.tag == "LavaCollider")
 		{
+			controller.SlowPlayer(0.4f);
 			happyMouth.SetActive(false);
 			sadMouth.SetActive(true);
 			rigidBody.velocity = Vector3.up * hurtBounceSpeed;
