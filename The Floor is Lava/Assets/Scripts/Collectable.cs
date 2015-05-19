755167755fd525962db1f4bd7fa123482e5754de
Isaac Blomfield <colias@live.com>
2015-05-20 00:15:07 +1200

    Added coins

--- The Floor is Lava/Assets/Scripts/Collectable.cs
+++ The Floor is Lava/Assets/Scripts/Collectable.cs
@@ -7,4 +7,16 @@ public class Collectable : MonoBehaviour {
 
 	public int value = 100;
 	public CollectableType type = CollectableType.Coin;
+
+	void OnTriggerEnter(Collider other)
+	{
+		print ("fuck" + other.gameObject.tag);
+		if(other.gameObject.tag == "Player")
+		{
+			print ("fuck");
+			EndlessController.controller.PickUp(this);
+			//TODO add some pretty pickup animation maybe
+			Destroy(this.gameObject);
+		}
+	}
 }
